3e261c2c8d2dd870b519a360194abf963d9c2666
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-04-20 10:15:28 +0300

    Initial Commit

---
+++ CombineFrameworkTutorial/CombineFrameworkTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineFrameworkTutorialApp.swift
+//  CombineFrameworkTutorial
+//
+//  Created by ramil on 20.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct CombineFrameworkTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
