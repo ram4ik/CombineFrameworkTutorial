94031082f857ad9f07e90b745e29eaeb1a5f2ab6
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-04-20 13:18:57 +0300

    tabView

--- CombineFrameworkTutorial/ContentView.swift
+++ CombineFrameworkTutorial/ContentView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        QuotesView()
+        TabView {
+            QuotesView()
+                .tabItem {
+                    Image(systemName: "quote.bubble")
+                    Text("Quotes")
+                }
+        }.accentColor(.secondary)
     }
 }
 
